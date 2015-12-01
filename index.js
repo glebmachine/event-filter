e898ea6ceaf91604f670993ceb8cb48c9ecb5c44
Gleb Mikheev <mikheev@betaagency.ru>
2015-12-01 18:06:31 +0300

    jshint

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
 function createFilter(fn, getter, initial){
   var _value = initial;
 
   if (getter) {
     return function(){
       var value = getter();
-      if (_value === value) return;
+      if (_value === value) { return; }
       _value = value;
       fn(value);
-    }
+    };
   }
 
   return function(value){
-    if (_value === value) return;
+    if (_value === value) { return; }
     _value = value;
     fn(value); 
-  }
+  };
 }
